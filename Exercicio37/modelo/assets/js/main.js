636f772829ec1341f3886d26242b869f177933b7
PedroYgor <pedroygor2010@hotmail.com>
2020-08-27 00:46:25 -0300

    teste

--- Exercicio37/modelo/assets/js/main.js
+++ Exercicio37/modelo/assets/js/main.js
@@ -1,6 +1,6 @@
 function meuEscopo(){
     const form = document.querySelector('.formulario');
-    const resultado = document.querySelector('.resultado');
+    const resultado = document.querySelector('#resultado');
     
     
     function receberEventoForm(evento){
