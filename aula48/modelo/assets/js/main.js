846a798bb235e9abc84964474c4ab19564a1b614
PedroYgor <pedroygor2010@hotmail.com>
2020-09-10 19:20:09 -0300

    Começando a aprender DOM

--- aula48/modelo/assets/js/main.js
+++ aula48/modelo/assets/js/main.js
@@ -4,12 +4,17 @@ const elementos = [
     {tag: 'footer', texto: 'Frase 3'},
     {tag: 'section', texto: 'Frase 4'}
 ];
-//const resultado = document.querySelector('.container');
+const resultado = document.querySelector('.container');
+const div = document.createElement('div');
+
 elementos.forEach(element => {
     let {tag, texto} = element;
-    
+    let criarTag = document.createElement(tag);
+    let criarTexto = document.createTextNode(texto);
+    criarTag.appendChild(criarTexto);
+    div.appendChild(criarTag);
 });
-
+resultado.appendChild(div);
 //elementos.forEach(elementos => {
 //    resultado.innerHTML += `<${elementos.tag}> ${elementos.texto} </${elementos.tag}>`;
 //}); 
