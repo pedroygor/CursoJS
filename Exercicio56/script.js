06d77a198b0444123cdf5e12843f78b1fe6cd3f9
PedroYgor <pedroygor2010@hotmail.com>
2020-09-11 18:59:41 -0300

    NodeList

---
+++ Exercicio56/script.js
@@ -0,0 +1,5 @@
+//Em pé Retrato, deitado paisagem
+
+const ePaisagem = (largura, altura) => largura > altura;
+
+console.log(ePaisagem(500, 450));
---
+++ exercicio55/script.js
@@ -0,0 +1,8 @@
+function maiorQue(num1, num2) {
+    return num1 >= num2 ? num1 : num2;
+}
+const max = (num1, num2) => num1 >= num2 ? num1 : num2; 
+
+
+let teste = max(1, 20);
+console.log(teste);
